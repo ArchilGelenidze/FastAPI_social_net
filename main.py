e4d94697753ee1fd7df8b31bc457e2ac5728abd2
Archie <mainpost.ge@gmail.com>
2025-08-25 16:45:04 +0400

    - Basic handlers were set - Basic HTTP exceptions were set

---
+++ main.py
@@ -0,0 +1,75 @@
+from fastapi import FastAPI, status, HTTPException, Response
+from fastapi.params import Body
+from pydantic import BaseModel
+from random import randrange
+
+app = FastAPI()
+
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: int | None = None
+
+
+my_posts = [{
+    "title": "The title of post 1",
+    "content": "Content of post 1",
+    "id": 1},
+    {"title": "Favourite food",
+     "content": "My favourite food is shawerma and pizza",
+     "id": 2}
+]
+
+def find_post(id):
+    for p in my_posts:
+        if p["id"] == id:
+            return p
+
+def find_post_index(id: int):
+    for indx, p in enumerate(my_posts):
+        if p["id"] == id:
+            return indx
+        
+
+@app.get("/")
+async def root():
+    return {"message": "Hello world"}
+
+@app.get("/posts")
+async def get_posts():
+    return {"data": my_posts}
+
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
+async def create_post(post: Post):
+    post_dict = post.model_dump()
+    post_dict["id"] = randrange(0, 1000000)
+    my_posts.append(post_dict)
+    return {"data": post_dict}
+
+@app.get("/posts/latest")
+async def get_latest_post():
+    post = my_posts[len(my_posts) - 1]
+    return {"detail": post}
+
+
+@app.get("/posts/{id}")
+async def get_post(id: int):
+    post = find_post(id)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"post with id {id} was not found")
+
+    return {"post_detail": post}
+
+
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_post(id: int):
+    index = find_post_index(id)
+    if index is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"post with id: {id} does not exist")
+
+    my_posts.pop(index)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
