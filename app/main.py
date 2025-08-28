3100f2bafd6c03164413345573eb83cb8d52bd71
Archie <mainpost.ge@gmail.com>
2025-08-28 17:26:14 +0400

    - Passwords hash was added - Hash function was added into utils

--- app/main.py
+++ app/main.py
@@ -2,14 +2,19 @@ from typing import List
 from fastapi import FastAPI, status, HTTPException, Response, Depends
 from fastapi.params import Body
 from pydantic import BaseModel
-import psycopg2
+# import psycopg2
 from psycopg2.extras import RealDictCursor
 from time import sleep
-from . import models, schemas
+from . import models, schemas, utils
 from .database import engine, get_db
 from sqlalchemy.orm import Session
 
+
+
+
 models.Base.metadata.create_all(bind=engine)
+
+
 
 app = FastAPI()
 
@@ -136,9 +141,25 @@ async def update_post(id: int, post: schemas.PostCreate, db: Session = Depends(g
 @app.post("/users", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
 async def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     
+    #hash the password - user.password
+    hashed_password = utils.hash(user.password)
+    user.password = hashed_password
+
+
     new_user = models.User(**user.model_dump())
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
 
     return new_user
+
+
+@app.get("/users/{id}", response_model=schemas.UserOut)
+async def get_user(id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"User with id: {id} does not exist")
+    
+
+    return user
